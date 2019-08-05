74d3f6092971dc2f618cb5664a65c88d00dd7e76
Valronn <charliescenek@gmail.com>
2019-08-05 18:52:21 +0300

    Finished Screenshots section

--- index.php
+++ index.php
@@ -2,46 +2,137 @@
 
 <div class="features">
     <div class="container">
-        <h1 class="display-3 title text-center">
+        <h1 class="display-4 title text-center">
             <b>PERFECT FEATURES</b>
         </h1>
         <h6 class="text-muted text-center bot_title">Only necessery</h6>
         <div class="row d-flex justify-content-center align-items-center">
             <div class=" row col-lg-4 col-md-12 col-sm-12 mb-5">
-               <img class="img-fluid" src="<?php echo get_bloginfo('template_directory'); ?> /images/features/images/icon_07.jpg" />
-               <div class="col">
-                <h6><b>Usability</b></h6>
-                <p class="text-muted">Sometimes the simplest thins are
-                    the hardest to find. So we created a 
-                    new line for everyday life
-                </p> 
-               </div>
+                <img class="img-fluid"
+                    src="<?php echo get_bloginfo('template_directory'); ?> /images/features/images/icon_07.jpg" />
+                <div class="col">
+                    <h6><b>Usability</b></h6>
+                    <p class="text-muted">Sometimes the simplest thins are
+                        the hardest to find. So we created a
+                        new line for everyday life
+                    </p>
+                </div>
             </div>
             <div class="row col-lg-4 col-md-12 col-sm-12 mb-5">
-                    <img class="img-fluid" src="<?php echo get_bloginfo('template_directory'); ?> /images/features/images/icon_09.jpg" />
-                    <div class="col">
-                     <h6><b>Parallax Effect  </b></h6>
-                     <p class="text-muted">Sometimes the simplest thins are
-                         the hardest to find. So we created a 
-                         new line for everyday life
-                     </p> 
-                    </div>
-                 </div>
-                 <div class="row col-lg-4 col-md-12 col-sm-12 mb-5">
-                        <img class="img-fluid" src="<?php echo get_bloginfo('template_directory'); ?>/images/features/images/icon_11.jpg" />
-                        <div class="col">
-                         <h6><b>Unlimited Colors</b></h6>
-                         <p class="text-muted">Sometimes the simplest thins are
-                             the hardest to find. So we created a 
-                             new line for everyday life
-                         </p> 
-                        </div>
-                     </div>
-            
-            
-
+                <img class="img-fluid"
+                    src="<?php echo get_bloginfo('template_directory'); ?> /images/features/images/icon_09.jpg" />
+                <div class="col">
+                    <h6><b>Parallax Effect </b></h6>
+                    <p class="text-muted">Sometimes the simplest thins are
+                        the hardest to find. So we created a
+                        new line for everyday life
+                    </p>
+                </div>
+            </div>
+            <div class="row col-lg-4 col-md-12 col-sm-12 mb-5">
+                <img class="img-fluid"
+                    src="<?php echo get_bloginfo('template_directory'); ?>/images/features/images/icon_11.jpg" />
+                <div class="col">
+                    <h6><b>Unlimited Colors</b></h6>
+                    <p class="text-muted">Sometimes the simplest thins are
+                        the hardest to find. So we created a
+                        new line for everyday life
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
+</div>
+<div class="widgets">
+    <div class="container">
+        <h1 class="display-4 title text-center">
+            <b>SIMPLE WIDGETS</b>
+        </h1>
+        <h6 class="text-muted text-center bot_title">Drag and drop</h6>
+        <div class="row  justify-content-center">
+            <div id="carouselExampleControls" class="carousel slide col-sm-10 col-lg-10" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block img-fluid"
+                            src="<?php echo get_bloginfo('template_directory'); ?>/images/widgets/images/widget_03.jpg"
+                            alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block img-fluid"
+                            src="<?php echo get_bloginfo('template_directory'); ?>/images/widgets/images/widget_03.jpg"
+                            alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block img-fluid"
+                            src="<?php echo get_bloginfo('template_directory'); ?>/images/widgets/images/widget_03.jpg"
+                            alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="control-prev" aria-hidden="true"><i class="fas fa-chevron-left"></i></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="control-next" aria-hidden="true"><i class="fas fa-chevron-right"></i></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class='screenshots'>
+    <div class="container">
+        <h1 class="display-4 title text-center">
+            <b>SCREENSHOTS</b>
+        </h1>
+        <h6 class="text-muted text-center bot_title">The brightest images</h6>
+        <div class='row '>
+            <div class="col-lg-6 col-xl-6 col-sm-6 col-md-6 ">
+                <img class="img-fluid mb-4"
+                    src="<?php echo get_bloginfo('template_directory'); ?>/images/screenshots_phones/images/phone_03.jpg" />
+            </div>
+            <div class="col-lg-6 col-xl-6 col-sm-6 col-md-6 align-self-center">
+
+                <img class="img-fluid" src="<?php echo get_bloginfo('template_directory'); ?>/images/screenshots_icons/images/icon_03.jpg" />
+                <h1 class="text-title my-3"><b>WHEN THE CLOUDS</b></h1>
+                <p>Variable information on the air humidity, the 
+                    feeling of the weather, and the ability to
+                    share this with your friends
+                </p>
+            </div>
+        </div>
+            <div class='middle-row row '>
+<div class="col-lg-6 col-xl-6 col-sm-6 col-md-6 align-self-center">
+                        <img class="img-fluid" src="<?php echo get_bloginfo('template_directory'); ?>/images/screenshots_icons/images/icon_11.jpg" />
+                        <h1 class="text-title my-3"><b>WHEN THE SUN</b></h1>
+                        <p>Sometimes the simplest thins are
+                                the hardest to find. So we created a
+                                new line for everyday life
+                        </p>
+                    </div>
+                    <div class="col-lg-6 col-xl-6 col-sm-6 col-md-6 ">
+                        <img class="img-fluid"
+                            src="<?php echo get_bloginfo('template_directory'); ?>/images/screenshots_phones/images/phone_07.jpg" />
+                    </div>
+                    
+                    
+                </div>
+                <div class='row '>
+                        <div class="col-lg-6 col-xl-6 col-sm-6 col-md-6 ">
+                            <img class="img-fluid mb-4"
+                                src="<?php echo get_bloginfo('template_directory'); ?>/images/screenshots_phones/images/phone_11.jpg" />
+                        </div>
+                        <div class="col-lg-6 col-xl-6 col-sm-6 col-md-6 align-self-center">
+            
+                            <img class="img-fluid" src="<?php echo get_bloginfo('template_directory'); ?>/images/screenshots_icons/images/icon_07.jpg" />
+                            <h1 class="text-title my-3"><b>WHEN THE CLOUDS</b></h1>
+                            <p>Sometimes the simplest thins are
+                                    the hardest to find. So we created a
+                                    new line for everyday life
+                            </p>
+                        </div>
+                    </div>
+        </div>
 </div>
 
 <?php get_footer(); ?>

fe6883d03e7baab23e37cf269a24b28bcb1d6ea8
Valronn <charliescenek@gmail.com>
2019-08-05 20:35:26 +0300

    Finished footer

--- footer.php
+++ footer.php
@@ -1,8 +1,36 @@
+<footer>
+  <div class="download-app">
+    <div class="container">
+      <div class="row pre-footer">
+        <div class="col col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 ml-md-5 ml-sm-0 ml-xs-0 ml-lg-0 ml-xl-0">
+          <h3 class="white-text"><b>HOW DOWNLOAD THE APP?</b></h3>
+          <p class="text-left white-text">Just download the app from the store.<br />
+            Simple, nice and user-frindly application of<br />
+            the weather. Only relevant and useful<br />
+            information.
+          </p>
+          <div class="d-flex flex-row">
+            <a href='#'><img class="img-fluid mr-2"
+                src='<?php echo get_bloginfo("template_directory") ?>./images/footer_phones/images/button_03.jpg' /></a>
+            <a href='#'><img class="img-fluid"
+                src='<?php echo get_bloginfo("template_directory") ?>./images/footer_phones/images/button_05.jpg' /></a>
+          </div>
+        </div>
+        <div class="col-6 d-none d-lg-block">
+          <img class="img-fluid"
+            src="<?php  echo get_bloginfo('template_directory'); ?>./images/footer_phones/images/3phones_03.jpg" />
+        </div>
+      </div>
+
+    </div>
 
 
-    <footer> <h3>this is footer.php</h3></footer>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
-    <?php wp_footer(); ?>
-  </body>
+  </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script>$(document).ready(function () { $('body').bootstrapMaterialDesign(); });</script>
+<?php wp_footer(); ?>
+</body>
+
 </html>
